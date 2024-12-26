4bbc0c2e03e5a23acfa5b0f4683a4e5f7bf23e08
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-12-25 19:26:54 -0800

    Bored API with axios

--- index.js
+++ index.js
@@ -16,12 +16,66 @@ function Person (first, last, age, eye){
     this.eyeColor = eye;
 }
 
-const myFather = new Person("john", "Cho", 56, "brown");
 
-myFather.job = "structural Engineer";
 
-console.log(myFather)
+// ES6 Key concepts
+// Logical &&
 
-let text  = `Ino Chika Cho is my favourite duo so far`;
-let char = text.substr(4,10)
-console.log(char)
+function getName(name) {
+    return name;
+}
+
+// console.log(getName('Miracle Sadu'))
+
+// it will check  the two values if the first is true it will run the second else it wouldn't move to the second 
+
+// Logical || one has to be true
+
+// Template literals 
+
+const PeoplesArray = [
+    {
+        name: "Anna Flickr",
+        age: 25,
+        country: "Nigeria",
+        isMarried: {
+            value: true,
+            spouse: "Behingham"
+        }
+    },
+    {
+        name: "Anna Flickr",
+        age: 25,
+        country: "Nigeria",
+        isMarried: {
+            value: true,
+            spouse: "Behingham"
+        }
+    },
+    {
+        name: "Victoria Jestins",
+        age: 25,
+        country: "Kenya",
+        isMarried: {
+            value: true,
+            spouse: "Behingham"
+        }
+    },
+    {
+        name: "Benard berkline",
+        age: 25,
+        country: "Nigeria",
+        isMarried: false,
+        gender: "male"
+    },
+    {
+        name: "Benard berkline",
+        age: 25,
+        country: "Nigeria",
+        isMarried: false,
+        gender: "male"
+    },
+]
+
+
+// here's a quick task for today, create an array of people some are single some are married match two random single people together, and the man should be older, and it should be a cross gender too 
